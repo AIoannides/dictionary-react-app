7f38dc28a214197e737f12575533597f4de593e7
Andriana Ioannides <ioannidou.aa@gmail.com>
2021-04-27 14:54:25 +0100

    Added Footer and Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+import Dictionary from "./Dictionary";
+import Footer from "./Footer";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img
-          src="https://images.unsplash.com/photo-1585521551422-497df464aa43?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
-          alt="open-book"
-          className="App-logo"
-        />
-        <h1>Dictionary</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img
+            src="https://images.unsplash.com/photo-1585521551422-497df464aa43?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
+            alt="open-book"
+            className="App-logo"
+          />
+          <h1>Dictionary</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,26 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <p>
+        Created by A.A.Ioannides, open sourced on{" "}
+        <a
+          href="https://github.com/AIoannides/dictionary-react-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://eloquent-bassi-0bf13d.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </p>
+    </div>
+  );
+}
