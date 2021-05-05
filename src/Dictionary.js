d2683ed1895324ca76dfdcabbfe9ca4c7b35ac19
Andriana Ioannides <ioannidou.aa@gmail.com>
2021-05-05 18:06:01 +0100

    Added pexels api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -12,16 +12,21 @@ export default function Dictionary() {
     setResults(response.data[0]);
   }
 
+  function handlePexelsResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${searchWord}`;
     axios.get(apiUrl).then(handleResponse);
 
-    // let pexelApiKey =
-    //   "563492ad6f91700001000001aacf4c1701044b84b18b5cf49c70a7b1";
-    // let pexelsUrl = `https://api.pexels.com/v1/search?query=${searchWord}&per_page=1`;
-    // axios.get(pexelUrl).then(handlePexelsResponse);
+    let pexelsApiKey =
+      "563492ad6f91700001000001aacf4c1701044b84b18b5cf49c70a7b1";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${searchWord}&per_page=1`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleChange(event) {
