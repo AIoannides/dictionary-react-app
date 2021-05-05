e16206b2e4726829d3e7fa1568c5713be16e4e4a
Andriana Ioannides <ioannidou.aa@gmail.com>
2021-05-05 19:34:20 +0100

    Deleted pexels api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -12,21 +12,11 @@ export default function Dictionary() {
     setResults(response.data[0]);
   }
 
-  function handlePexelsResponse(response) {
-    console.log(response.data);
-  }
-
   function search(event) {
     event.preventDefault();
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${searchWord}`;
     axios.get(apiUrl).then(handleResponse);
-
-    let pexelsApiKey =
-      "563492ad6f91700001000001aacf4c1701044b84b18b5cf49c70a7b1";
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${searchWord}&per_page=1`;
-    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
-    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleChange(event) {
@@ -35,12 +25,11 @@ export default function Dictionary() {
 
   return (
     <div className="Dictionary">
-      <form className="search" onSubmit={search}>
+      <form onSubmit={search}>
         <input
           type="Search"
           placeholder="Search for a word"
           onChange={handleChange}
-          className="search-input"
         />
       </form>
 
